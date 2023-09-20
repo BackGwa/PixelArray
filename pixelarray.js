e475f508f43b3567957817f03d72b871c93ad4a0
BackGwa <backgwa@icloud.com>
2023-09-20 18:03:13 +0900

    색상 지원을 위한 1차 수정

--- pixelarray.js
+++ pixelarray.js
@@ -1,3 +1,5 @@
+let enable = false;
+
 function create_dot(){
     dot = document.querySelector("dot-area");
     for (var i = 0; i < 32; i++) {
@@ -24,10 +26,10 @@ function create_dot_item(){
 
 function item_fill(e) {
     if(e.buttons >= 1 || e.type == "click") {
-        if(!e.target.classList.contains("fill")) {
-            e.target.classList.add("fill");
+        if(!e.target.classList.contains("c0")) {
+            e.target.classList.add("c0");
         } else {
-            e.target.classList.remove("fill");
+            e.target.classList.remove("c0");
         }
     }
 }
@@ -60,24 +62,27 @@ function draw(result, dot_item, index) {
     setTimeout(() => {
         if(index == 1023) {
             setTimeout(() => {
-                dot_item[index].classList.replace("fill_g", "fill");
+                dot_item[index].classList.replace("fill_g", "c0");
+                enable = true;
             }, index * 1);
         }
 
         if(index != 0) {
             setTimeout(() => {
-                dot_item[index - 1].classList.replace("fill_g", "fill");
+                dot_item[index - 1].classList.replace("fill_g", "c0");
             }, index * 1);
         }
 
         if(result == "1") {
             dot_item[index].classList.add("fill_g");
-            playse("drawse");
         }
 
         if(result == "2") {
-            dot_item[index].classList.remove("fill");
-            playse("drawse");
+            dot_item[index].classList.remove("c0");
+        }
+
+        if((index + 1) % 16 == 0) {
+            playsx(index);
         }
 
     }, index * 2);
@@ -108,9 +113,14 @@ function load_call() {
         });
     });
 
-    setTimeout(() => {
-        document.querySelector("cli-interface").classList.remove("disable");
-    }, 2048);
+    T = setInterval(() => {
+        if(enable) {
+            document.querySelector("cli-interface").classList.remove("disable");
+            enable = false;
+            clearInterval(T);
+            playse("btnse");
+        }
+    }, 256);
 
     close_dialog();
 }
@@ -126,7 +136,7 @@ function copy() {
 
     for (var i = 0; i < 32; i++) {
         for (var j = 0; j < 32; j++) {
-            if(dot_item[idx].classList.contains("fill")) v = 1;
+            if(dot_item[idx].classList.contains("c0")) v = 1;
             else v = 0;
             
             if(j == 0)
@@ -161,15 +171,26 @@ function copy() {
 
 function revert() {
     playse('ckse');
+    document.querySelector("cli-interface").classList.add("disable");
     dot_item = document.querySelectorAll("dot-item");
 
     dot_item.forEach((item, index) => {
-        if(item.classList.contains("fill")) {
+        if(item.classList.contains("c0")) {
             draw("2", dot_item, index);
         } else {
             draw("1", dot_item, index)
         };
     });
+
+    T = setInterval(() => {
+        if(enable) {
+            document.querySelector("cli-interface").classList.remove("disable");
+            enable = false;
+            clearInterval(T);
+            playse("btnse");
+        }
+    }, 256);
+    
 }
 
 function close_dialog() {
@@ -196,4 +217,24 @@ create_dot();
 function playse(type) {
     document.querySelector("#se").src = `./${type}.mp3`;
     document.querySelector("#se").play();
+}
+
+function playsx(idx) {
+    sx = document.createElement("audio");
+
+    sx.setAttribute('loop', ``);
+    sx.setAttribute('src', `./drawse.mp3`);
+    sx.setAttribute('id', `sx${idx}`);
+    sx.setAttribute('class', `sxaudio`);
+
+    document.body.appendChild(sx);
+    document.getElementById(`sx${idx}`).play();
+
+    setTimeout(() => {
+        document.getElementById(`sx${idx}`).remove();
+    }, 500);
+}
+
+document.oncontextmenu = () => {
+    return false;
 }
